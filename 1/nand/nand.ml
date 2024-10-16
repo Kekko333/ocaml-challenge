2abc78f575783f4791e52c2408047011bb2b4735
Francesco Mameli <f.mameli62@studenti.unica.it>
2024-10-16 21:27:21 +0200

    aggiundo tutto

--- 1/nand/nand.ml
+++ 1/nand/nand.ml
@@ -1,4 +1,8 @@
-let nand a b : bool = match(a,b) with
+let nand1 a b : bool = not(a && b);;
+
+let nand2 a b: bool = (not a) || (not b);;
+
+let nand3 a b : bool = match(a,b) with
 |(false, false) -> true
 |(false, true) -> true
 |(true, false) -> true
