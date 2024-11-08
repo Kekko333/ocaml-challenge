e864dbfcc30546a4201592699c17464af0b06d4b
Francesco Mameli <f.mameli62@studenti.unica.it>
2024-11-08 11:51:13 +0100

    implementazione mux4

--- 1/mux/mux.ml
+++ 1/mux/mux.ml
@@ -7,3 +7,11 @@ let mux2b s0 a b=
 let mux2c s0 a b= match s0 with
 | true -> a
 | false -> b;;
+
+
+let mux2 s0 a b = if s0 then a else b;;
+
+let mux4 s1 s0 a0 a1 a2 a3 =
+  let selected_lower = mux2 s0 a0 a1 in
+  let selected_upper = mux2 s0 a2 a3 in
+  mux2 s1 selected_lower selected_upper;;
